9fa39573f18b8a09f8b646284a087eb6eff9a97f
mmoyaa <mmoyaxd@gmail.com>
2023-05-28 19:00:27 -0400

    commit de la niche

---
+++ Clinica/Areas/mvcControlador/mvcControladorAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Clinica.Areas.mvcControlador
+{
+    public class mvcControladorAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "mvcControlador";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "mvcControlador_default",
+                "mvcControlador/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Clinica/Modelo/Model1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clinica.Modelo
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
